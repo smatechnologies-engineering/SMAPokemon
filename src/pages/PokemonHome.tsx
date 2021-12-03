e4d508ecf57eb72cc7cf83200c03f2466a42318f
Ryan Schlueter <Schlueter_ryan@outlook.com>
2021-12-02 20:09:58 -0600

    Add home page

---
+++ src/pages/PokemonHome.tsx
@@ -0,0 +1,9 @@
+import Container from '@mui/material/Container'
+
+export function PokemonHome() {
+  return (
+    <Container style={{ paddingBottom: 24, paddingTop: 24 }} maxWidth={'lg'}>
+      Home Page
+    </Container>
+  )
+}
