5ccf3974a271fb33cf8ca760b7937a36f579b0b9
Ryan Schlueter <Schlueter_ryan@outlook.com>
2021-12-02 20:09:47 -0600

    Add tests for pokedex page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { render, screen, fireEvent } from '@testing-library/react'
 import App from './App'
 
-test('it renders learn react link', () => {
+test('Go To Search Page', () => {
   render(<App />)
+  fireEvent.click(screen.getByText('Search'))
+
   const linkElement = screen.getByText(/Find your Pokemon/i)
+  expect(linkElement).toBeInTheDocument()
+})
+
+//would mock out the api calls in real case
+test('Search on search page', () => {
+  render(<App />)
+  fireEvent.click(screen.getByText('Search'))
+  const input = screen.getByTestId('search-input')
+  fireEvent.change(input, { target: { value: '12' } })
+  fireEvent.click(screen.getByText('Submit'))
+
+  const linkElement = screen.getByText('poison')
   expect(linkElement).toBeInTheDocument()
 })
