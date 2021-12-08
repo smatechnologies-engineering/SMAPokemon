977e7ab18e88945b5e1137703b97cb02b153b07a
Aaron Lu <aaronnoh0409@gmail.com>
2021-12-08 13:28:31 -0500

    relocating files so its a little more organized

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import { Pokedex } from './pages/Pokedex'
 import { PokemonSearch } from './pages/PokemonSearch'
 import { BrowserRouter, Router, Route, Link, Switch } from 'react-router-dom'
-import logo from './Pokemon-Logo.png'
+import logo from './components/Logo/Pokemon-Logo.png'
 
 function App() {
   return (
