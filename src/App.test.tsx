8e1e7fe6f0791e17ac22b61c0c491eb955e74db5
Aaron Lu <aaronnoh0409@gmail.com>
2021-12-12 21:30:48 -0500

    figuring out how to write tests with jest and react

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App'
 
 test('it renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/Find your Pokemon/i)
-  expect(linkElement).toBeInTheDocument()
+  const h1Element = screen.getByText(/hi/i)
+  expect(h1Element).toBeInTheDocument()
 })
