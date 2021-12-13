a199660f04ad58d559fe5c6c7712d88e3752f353
Aaron Lu <aaronnoh0409@gmail.com>
2021-12-12 21:47:37 -0500

    added two tests to check if the app rendered logo and the texts

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import App from './App'
 
-test('it renders learn react link', () => {
+test('it renders pokemon logo', () => {
   render(<App />)
-  const h1Element = screen.getByText(/hi/i)
+  const imgElement = screen.getByAltText(/Logo/i);
+  expect(imgElement).toBeInTheDocument();
+})
+
+test('it renders main pages texts', () => {
+  render(<App />)
+  const h1Element = screen.getByText(/Let's find your FAVORITE Pokemon!/i)
   expect(h1Element).toBeInTheDocument()
 })
