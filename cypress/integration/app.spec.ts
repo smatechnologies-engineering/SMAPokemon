55b400ede15a122ad4f3747c723a2e6dfb1e9529
jackgilbertx <jackgilbertx@gmail.com>
2022-01-25 00:03:28 -0600

    Add files via upload

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -1,7 +1,11 @@
-//
-describe('first visit', () => {
-  it('works', () => {
+describe('test search', () => {
+  it('navigates to search and enters search text', () => {
     cy.visit('localhost:3001')
-    cy.get('input').type('pikachu{enter}')
+    cy.get('#search-link').click()
+    cy.url().should('include', 'search')
+    cy.get('input').type('pikachu')
+    // app errors will crash this test, also .wait is flakey
+    cy.wait(3000)
+    cy.get('.MuiCard-root').should('exist')
   })
 })
