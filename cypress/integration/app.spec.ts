9796de095b4ce572d95fa86e99ea3107f9a74296
jackgilbertx <jackgilbertx@gmail.com>
2022-01-26 04:56:16 -0600

    adding tests

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -1,10 +1,22 @@
 describe('test search', () => {
-  it('navigates to search and enters search text', () => {
+  before(() => {
     cy.visit('localhost:3001')
     cy.get('#search-link').click()
+  })
+
+  it('navigates to search correctly', () => {
     cy.url().should('include', 'search')
+  })
+
+  it('should find and show pokemon', () => {
     cy.get('input').type('pikachu')
     cy.get('form').submit()
     cy.get('.MuiCard-root').should('exist')
+  })
+
+  it('should render error message', () => {
+    cy.get('input').type('pdsdfhusfd')
+    cy.get('form').submit()
+    cy.get('.MuiAlert-message').should('exist')
   })
 })
