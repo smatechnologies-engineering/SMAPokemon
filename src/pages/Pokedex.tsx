fac6692ee59f7cd5a1ee6043c0dd2a972fff700a
ZeroJimmy100 <jpjimmy09@gmail.com>
2022-01-30 23:04:28 -0800

    add a load spinner and pagination

--- src/pages/Pokedex.tsx
+++ src/pages/Pokedex.tsx
@@ -1,7 +1,9 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
 import { PokemonInfoCard } from '../components/PokemonInfoCard'
+import '../styles/pages/PokeDex.css'
+import Pagination from '@mui/material/Pagination'
 
 interface Pokemon {
   name: string
@@ -10,6 +12,17 @@ interface Pokemon {
 
 export function Pokedex() {
   const [pokemon, setPokemon] = useState<Array<Pokemon>>([])
+  const [page, setPage] = React.useState<number>(1)
+  const [itemsPerPage, setItemsPerPage] = React.useState<number>(6)
+
+  const handleChange = (
+    event: React.ChangeEvent<unknown> | null,
+    newPage: number
+  ) => {
+    setPage(newPage)
+  }
+
+  const noOfPages = Math.ceil(pokemon.length / itemsPerPage)
 
   useEffect(() => {
     ;(async function getPokemon() {
@@ -19,19 +32,30 @@ export function Pokedex() {
     })()
   }, [])
   return (
-    <Container style={{ paddingBottom: 24, paddingTop: 24 }} maxWidth={'lg'}>
-      <Grid
-        container
-        spacing={{ xs: 2, md: 3 }}
-        columns={{ xs: 4, sm: 8, md: 12 }}
-      >
-        {pokemon.map((p) => (
-          <Grid item xs={4} sm={4} md={4} key={p.name}>
-            <PokemonInfoCard pokemon={p} />
-            <br />
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
+    <div className="bgColor">
+      <Container style={{ paddingBottom: 24, paddingTop: 100 }} maxWidth={'lg'}>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+        >
+          {pokemon
+            .slice((page - 1) * itemsPerPage, page * itemsPerPage)
+            .map((p) => (
+              <Grid item xs={4} sm={4} md={4} key={p.name}>
+                <PokemonInfoCard pokemon={p} />
+                <br />
+              </Grid>
+            ))}
+        </Grid>
+        <Pagination
+          count={noOfPages}
+          page={page}
+          defaultPage={1}
+          onChange={handleChange}
+          color="primary"
+        />
+      </Container>
+    </div>
   )
 }
