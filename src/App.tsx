9662e78bcc36fd89409d88d8696dbbd9bdc42466
ZeroJimmy100 <jpjimmy09@gmail.com>
2022-01-30 20:44:43 -0800

    Add route path for other urls

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import { ChangeEvent, useState, useEffect } from 'react'
 import './App.css'
 import { Pokedex } from './pages/Pokedex'
 import { PokemonSearch } from './pages/PokemonSearch'
-import { BrowserRouter, Router, Route, Link, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { PokemonCard } from './components/Pokemon'
+import { HomePage } from './pages/HomePage'
+import { PokeNav } from './components/PokeNav'
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <Switch>
-            <Route path="/pokedex">
-              <Pokedex />
-            </Route>
-            <Route exact path="/">
-              <PokemonSearch />
-            </Route>
-            <Route path="/:pokemon">{/* <PokemonCard /> */}</Route>
-          </Switch>
-        </header>
-      </div>
+      <PokeNav />
+      <Switch>
+        <Route path="/pokedex">
+          <Pokedex />
+        </Route>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/poke-search">
+          <PokemonSearch />
+        </Route>
+        <Route path="/info/:pokemon">
+          <PokemonCard />
+        </Route>
+      </Switch>
     </BrowserRouter>
   )
 }
