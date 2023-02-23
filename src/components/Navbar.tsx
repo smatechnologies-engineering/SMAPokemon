d43661019b648ab1d8eebe4eaf50f9716307b959
juslong <juslong@umich.edu>
2023-02-23 11:17:39 -0700

    Updating navbaar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,45 @@
-import { AppBar, Button, Container, Stack, Toolbar } from '@mui/material'
-import { Link } from 'react-router-dom'
-import logo from '../assests/Pokemon-Logo-700x394.png'
+import { AppBar, Tab, Tabs, Toolbar } from '@mui/material'
+import { Link, useHistory } from 'react-router-dom'
+import PokemonLogo from '../assests/Pokemon-Logo-700x394.png'
 import '../App.css'
 
+type TabIndexMap = {
+  [id: number]: string
+}
+
 export function NavBar() {
+  const tabIndexToRoute: TabIndexMap = {
+    0: '/',
+    1: '/pokedex',
+  }
+
+  const history = useHistory()
+
+  const handleTabChange = (_event: React.SyntheticEvent, tabIndex: number) => {
+    history.push(tabIndexToRoute[tabIndex])
+  }
+
   return (
     <AppBar title="Pokemon Finder" position="static">
       <Toolbar>
         <Link to="/">
-          <img src={logo} alt="logo" className="navbar-logo" />
+          <img src={PokemonLogo} alt="logo" className="navbar-logo" />
         </Link>
-        <Stack
-          direction="row"
-          spacing={2}
+        <Tabs
+          onChange={handleTabChange}
           sx={{
             marginLeft: '20px',
+            '& button:focus': { borderBottom: '2px solid white' },
           }}
         >
-          <Link to="/" className="navbar-link">
-            Pokemon Search
-          </Link>
-          <Link to="/pokedex" className="navbar-link">
-            All Pokemon
-          </Link>
-        </Stack>
+          <Tab label="Pokemon Search" sx={{ color: '#fff' }} />
+          <Tab
+            label="All Pokemon"
+            sx={{
+              color: '#fff',
+            }}
+          />
+        </Tabs>
       </Toolbar>
     </AppBar>
   )
