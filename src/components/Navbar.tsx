02fa744b14fd031aacd55960d1257e9814180255
juslong <juslong@umich.edu>
2023-02-23 23:50:35 -0700

    Style navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,39 @@ import { AppBar, Tab, Tabs, Toolbar } from '@mui/material'
 import { Link, useHistory } from 'react-router-dom'
 import PokemonLogo from '../assests/Pokemon-Logo-700x394.png'
 import '../App.css'
+import { useState } from 'react'
+import { styled } from '@mui/system'
+
+const StyledTabs = styled(Tabs)({
+  '& .MuiTabs-indicator': {
+    backgroundColor: '#1890ff',
+  },
+})
+
+const StyledTab = styled((props: StyledTabProps) => (
+  <Tab disableRipple {...props} />
+))(() => ({
+  '&:hover': {
+    color: '#ffcb05',
+  },
+  '&.Mui-selected': {
+    color: '#fff',
+  },
+  '&.Mui-focusVisible': {
+    backgroundColor: '#d1eaff',
+  },
+}))
+
+interface StyledTabProps {
+  label: string
+}
 
 type TabIndexMap = {
   [id: number]: string
 }
 
 export function NavBar() {
+  const [tabIndex, setTabIndex] = useState(0)
   const tabIndexToRoute: TabIndexMap = {
     0: '/',
     1: '/pokedex',
@@ -17,29 +44,34 @@ export function NavBar() {
 
   const handleTabChange = (_event: React.SyntheticEvent, tabIndex: number) => {
     history.push(tabIndexToRoute[tabIndex])
+    setTabIndex(tabIndex)
   }
 
   return (
     <AppBar title="Pokemon Finder" position="static">
       <Toolbar>
-        <Link to="/">
+        <Link to="/" onClick={() => setTabIndex(0)}>
           <img src={PokemonLogo} alt="logo" className="navbar-logo" />
         </Link>
-        <Tabs
+        <StyledTabs
+          value={tabIndex}
           onChange={handleTabChange}
           sx={{
             marginLeft: '20px',
-            '& button:focus': { borderBottom: '2px solid white' },
+          }}
+          indicatorColor="secondary"
+          TabIndicatorProps={{
+            style: { backgroundColor: '#fff', color: '#fff' },
           }}
         >
-          <Tab label="Pokemon Search" sx={{ color: '#fff' }} />
-          <Tab
+          <StyledTab label="Pokemon Search" sx={{ color: '#fff' }} />
+          <StyledTab
             label="All Pokemon"
             sx={{
               color: '#fff',
             }}
           />
-        </Tabs>
+        </StyledTabs>
       </Toolbar>
     </AppBar>
   )
